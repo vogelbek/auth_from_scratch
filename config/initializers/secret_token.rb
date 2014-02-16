d10be0b83ed36cefebb5afd1890f649bac35653e
Adam Hendricksen <adam@nird.us>
2014-02-16 13:34:18 -0800

    Initializing rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthFromScratch::Application.config.secret_key_base = '4587e9cd619df848cdd58d93d18573112caf77dbd9baa0b8b25f2876d9ebe27a588335aa21004460c6f0a5a4536b140c314c34bf1c611fdb46e6c06d9f9efdf7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthFromScratch::Application.config.session_store :cookie_store, key: '_auth_from_scratch_session'
